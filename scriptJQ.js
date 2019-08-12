df7590c1ad2a24d85ecd086e2fdba75e0bb50984
GooseGus <barroso.gustavo.g@gmail.com>
2019-08-12 02:49:44 +0100

    correct space erroers

--- scriptJQ.js
+++ scriptJQ.js
@@ -4,18 +4,10 @@
 	
 		$(window).scroll(function(){
 			var este= $(document).height();
-			var htmltotal= $('html').height();
-			var bodytotal= $('body').height();
 			var wScroll=$(window).scrollTop();
 			var heightotal= $(window).outerHeight();
-			var stickyy= $('.sticky-top').height();
 			
-			console.log('htmltotal:: '+htmltotal);
-			console.log('bodytotal:: '+bodytotal);
-			console.log('document height:: '+este);
-			console.log('scroll:: '+wScroll);
-			console.log('window size:: '+heightotal);
-			console.log('                               ');
+        
         
 			var stickyTOP = $('.sticky-top');
 			var sticky = stickyTOP.offset().top;
@@ -25,11 +17,11 @@
 			if(wScroll>=sticky  && !stickyTOP.is(".navbar-fixed-top")){
 			
 				$('.sticky-top').addClass('navbar-fixed-top');
-				$('#ricee').css('margin-top',stickyy+"px");
+			//	$('#ricee').css('margin-top',stickyy+"px");
   
 			}else if(wScroll<heightotal){
 				$('.sticky-top').removeClass('navbar-fixed-top');
-				$('#ricee').css('margin-top',"0px");
+			//	$('#ricee').css('margin-top',"0px");
            
 			}
 		//navbar - scrool bar
@@ -38,14 +30,24 @@
 				$('.progress-bar').css('width',actual+"%");
 			}
 
+			$('.parallax-Wand').each(function(){
+				var BWandt = $(this).offset().top;
+				
+				if (wScroll > (BWandt-heightotal) && (BWandt > wScroll) && (wScroll+heightotal > BWandt)){
+					paraBG=wScroll-(BWandt-heightotal);
+					$(this).css('background-position','center '+(-paraBG*0.1)+'px')
+				} else {
+               // console.log( "im out" );
+				}  
+			});
+
 
 			$('.parallax-BG').each(function(){
 				var BGot = $(this).offset().top;
-				var BGoh = $(this).outerHeight();
 				
 				if (wScroll > (BGot-heightotal) && (BGot > wScroll) && (wScroll+heightotal > BGot)){
 					paraBG=wScroll-(BGot-heightotal);
-					$(this).css('background-position','center '+(-paraBG*0.2)+'px')
+					$(this).css('background-position','center '+(-paraBG*0.15)+'px')
 				} else {
                // console.log( "im out" );
 				}  
@@ -53,7 +55,6 @@
 
 			$('.parallax-IM').each(function(){
 				var IMot = $(this).offset().top;
-				var IMoh = $(this).outerHeight();
 
 				if (wScroll > (IMot-(2*heightotal)) && (IMot > wScroll) && ( wScroll+(2*heightotal) > IMot)){
 					paraIM=wScroll-(IMot-heightotal)
